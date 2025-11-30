c6c611342ec456442533dc1801f558c8aa94c99c
Treta5 <158505420+Treta5@users.noreply.github.com>
2025-11-30 18:32:11 +0000

    Forgot to save before commit.

--- src/trash_ship_simulator/universe-client.c
+++ src/trash_ship_simulator/universe-client.c
@@ -1,41 +1,7 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <zmq.h>
-
-struct contextDataforClosing{
-    void (*pFunction)(void *);
-    void *pArguments;
-    struct contextDataforClosing *pPreviousStruct;
-};
-
-void closeContexts(struct contextDataforClosing *Data){
-    printf("Executing close contexts\n");
-    (Data->pFunction)((void*)Data->pArguments);
-    printf("Completed close contexts\n");
-    printf("%p\n", Data->pPreviousStruct);
-    if((Data->pPreviousStruct)){
-        printf("Recurisng\n");
-        closeContexts(Data->pPreviousStruct);
-    }
-    free(Data);
-}
-
-struct contextDataforClosing *createContextDataforClosing(  void *pInFunction, void *pInArguments, 
-                                                        struct contextDataforClosing *pInPreviousStruct){
-
-    struct contextDataforClosing *newStruct = malloc(sizeof(struct contextDataforClosing));
-
-    if(!newStruct){
-        closeContexts(pInPreviousStruct);
-        printf("Failed to create programContext(malloc).\n");
-        return NULL;
-    }
-
-    newStruct->pFunction = pInFunction;
-    newStruct->pArguments = pInArguments;
-    newStruct->pPreviousStruct = pInPreviousStruct;
-    return newStruct;
-}
+#include "mylib.c"
 
 int main(int argc, char *argv[])
 {
