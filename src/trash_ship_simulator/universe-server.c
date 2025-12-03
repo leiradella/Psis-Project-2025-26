285679a2cb65a2a4e5e28fd070e87561351990a4
Treta5 <158505420+Treta5@users.noreply.github.com>
2025-12-03 11:43:32 +0000

    Creation of GUI
    G loop was not yet implemented.
    Tested, window does not open.
    Otherwise as expected.

--- src/trash_ship_simulator/universe-server.c
+++ src/trash_ship_simulator/universe-server.c
@@ -1,11 +1,88 @@
 #include <stdlib.h>
+#include <time.h>
+
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
 #include <zmq.h>
-#include "mylib.c"
 #include "msg.pb-c.h"
+
+
+#include "universe-data.h"
+#include "physics-rules.h"
+#include "display.h"
+
+#include "mylib.c"      //Adicionar a outras bibliotecas.
+
 
 int main(int argc, char *argv[])
 {
+
+    //seed random number generator with current time
+    srand(time(NULL));
+    int seed = rand(); //initialize seed with a random value
+
+    //create the universal initial state here using universe_config parameters
+    GameState* game_state = CreateInitialUniverseState("universe_config.conf", seed);
+
+    if (game_state == NULL) {
+        printf("Failed to create initial universe state. Exiting...\n");
+        return 1;
+    }
+
+    //Initalize SDL
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    struct contextDataforClosing *programContext = createContextDataforClosing(SDL_Quit, NULL, NULL);
+
+    if(!programContext) return -1;
+
+
+    //Initialize SDL_ttf
+    if (TTF_Init() != 0) {
+        printf("TTF_Init Error: %s\n", TTF_GetError());
+        closeContexts(programContext);
+        return 1;
+    }
+
+        //create SDL_window variable
+    SDL_Window *window = SDL_CreateWindow(
+        "Universe Simulator", 
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED, 
+        game_state->universe_size, 
+        game_state->universe_size, 
+        SDL_WINDOW_SHOWN
+    );
+
+    //check if window was created successfully
+    if (window == NULL) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        closeContexts(programContext);
+        return 1;
+    }
+
+    programContext = createContextDataforClosing(SDL_DestroyWindow, window, programContext);
+
+    if(!programContext) return -1;
+
+    //create SDL_renderer variable
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    if (renderer == NULL) {
+        SDL_DestroyWindow(window);
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        closeContexts(programContext);
+        return 1;
+    }
+    programContext = createContextDataforClosing(SDL_DestroyRenderer, renderer, programContext);
+
+    if(!programContext) return -1;
+
+
     //Create ZMQ_Context
     void *pContext = zmq_ctx_new();
 
@@ -14,13 +91,14 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    struct contextDataforClosing *programContext = createContextDataforClosing(zmq_ctx_destroy, pContext, NULL);
+    programContext = createContextDataforClosing(zmq_ctx_destroy, pContext, programContext);
 
     if(!programContext) return -1;
 
     //Socket to send messages to
     void *pReceive = zmq_socket(pContext, ZMQ_PULL);
     zmq_bind(pReceive, "tcp://localhost:5558");
+
     if(!pReceive){
         printf("error initializing ZMQ: %s\n", zmq_strerror(errno));
         closeContexts(programContext);
@@ -35,6 +113,7 @@ int main(int argc, char *argv[])
     zmq_msg_init(&zmq_msg);
     u_int8_t msg;
 
+    int running = 1;
     do
     {
         msg = 0;
@@ -54,10 +133,9 @@ int main(int argc, char *argv[])
 
         }
 
-        //buffer[1] = '\0';
+        //CheckEvents(&running, game_state)
 
-        //printf("%b\n", msg);
-    }while(1);
+    }while(running);
     
     closeContexts(programContext);
     return 0;
